8c8bf2d0aba8cc5b3f67d2d650e052c2da8ee96b
Seonghyeony <sunghyun7949@naver.com>
2021-01-02 00:21:27 +0900

    Update: Scope 개념 추가

--- Swift-basic/MyPlayground.playground/Contents.swift
+++ Swift-basic/MyPlayground.playground/Contents.swift
@@ -87,3 +87,22 @@ let jasonOrMale = isJason || isMale
 
 let greetingMessage: String = isJason ? "Hello Jason" : "Hello Somebody"
 print("Msg: \(greetingMessage)")
+
+
+// --- Scope: 변수의 범위
+
+
+var hours = 50
+let payPerHour = 10000
+var salary = 0
+
+if hours > 40 {
+    let extraHours = hours - 40
+    salary += extraHours * payPerHour * 2
+    hours -= extraHours
+}
+
+salary += hours * payPerHour
+
+//print(hours)
+
