2bb75ad3505c5502ef844a32555040bea4c65c18
Seonghyeony <sunghyun7949@naver.com>
2021-01-02 13:26:14 +0900

    Function 직접 만들어보기

--- Swift-basic/function_과제.playground/Contents.swift
+++ Swift-basic/function_과제.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var str = "Hello, playground"
+// 1. 성, 이름을 받아서 fullname을 출력하는 함수 만들기
+
+// 2. 1번에서 만든 함수인데, 파라미터 이름을 제거하고 fullname을 출력하는 함수 만들기
+
+// 3. 성, 이름을 받아서 fullname return 하는 함수 만들기
+
+/**
+ 1.
+ */
+func printFullName(firstName: String, lastName: String) {
+    print("FullName is \(firstName) \(lastName).")
+}
+
+printFullName(firstName: "Seonghyeon", lastName: "Lim")
+
+/**
+ 2.
+ */
+
+func printFullName_2(_ firstName: String, _ lastName: String) {
+    print("FullName is \(firstName) \(lastName).")
+}
+
+printFullName_2("Seonghyeon", "Lim")
+
+/**
+ 3.
+ */
+func returnFullName(_ firstName: String, _ lastName: String) -> String {
+    //return firstName + lastName
+    return "\(firstName) \(lastName)"
+}
+
+let fullName = returnFullName("Seonghyeon", "Lim")
+fullName
