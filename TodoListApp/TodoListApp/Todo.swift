bc43c29f82f41ddfe89d7214a73aa7420289606a
Seonghyeony <sunghyun7949@naver.com>
2021-01-11 22:42:00 +0900

    First Commit: TodoList app start and coding Todo Struct

---
+++ TodoListApp/TodoListApp/Todo.swift
@@ -0,0 +1,123 @@
+//
+//  Todo.swift
+//  TodoList
+//
+//  Created by joonwon lee on 2020/03/19.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+
+// TODO: Codable과 Equatable 추가
+struct Todo: Codable, Equatable {
+    let id: Int         // 구분자, PK
+    var isDone: Bool
+    var detail: String
+    var isToday: Bool
+    
+    mutating func update(isDone: Bool, detail: String, isToday: Bool) {
+        // [완료] TODO: update 로직 추가
+        self.isDone = isDone
+        self.detail = detail
+        self.isToday = isToday
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        /**
+         객체 간의 동등 비교를 하기 위해 Equatable 프로토콜을 따라야 하고,
+         '==' 연산자를 정의할 수 있다.
+        */
+        // [완료] TODO: 동등 조건 추가
+        return lhs.id == rhs.id
+    }
+}
+
+class TodoManager {
+    
+    static let shared = TodoManager()
+    
+    static var lastId: Int = 0
+    
+    var todos: [Todo] = []
+    
+    func createTodo(detail: String, isToday: Bool) -> Todo {
+        //TODO: create로직 추가
+        return Todo(id: 1, isDone: false, detail: "2", isToday: true)
+    }
+    
+    func addTodo(_ todo: Todo) {
+        //TODO: add로직 추가
+    }
+    
+    func deleteTodo(_ todo: Todo) {
+        //TODO: delete 로직 추가
+        
+    }
+    
+    func updateTodo(_ todo: Todo) {
+        //TODO: updatee 로직 추가
+        
+    }
+    
+    func saveTodo() {
+        Storage.store(todos, to: .documents, as: "todos.json")
+    }
+    
+    func retrieveTodo() {
+        todos = Storage.retrive("todos.json", from: .documents, as: [Todo].self) ?? []
+        
+        let lastId = todos.last?.id ?? 0
+        TodoManager.lastId = lastId
+    }
+}
+
+class TodoViewModel {
+    
+    enum Section: Int, CaseIterable {
+        case today
+        case upcoming
+        
+        var title: String {
+            switch self {
+            case .today: return "Today"
+            default: return "Upcoming"
+            }
+        }
+    }
+    
+    private let manager = TodoManager.shared
+    
+    var todos: [Todo] {
+        return manager.todos
+    }
+    
+    var todayTodos: [Todo] {
+        return todos.filter { $0.isToday == true }
+    }
+    
+    var upcompingTodos: [Todo] {
+        return todos.filter { $0.isToday == false }
+    }
+    
+    var numOfSection: Int {
+        return Section.allCases.count
+    }
+    
+    func addTodo(_ todo: Todo) {
+        manager.addTodo(todo)
+    }
+    
+    func deleteTodo(_ todo: Todo) {
+        manager.deleteTodo(todo)
+    }
+    
+    func updateTodo(_ todo: Todo) {
+        manager.updateTodo(todo)
+    }
+    
+    func loadTasks() {
+        manager.retrieveTodo()
+    }
+}
+
