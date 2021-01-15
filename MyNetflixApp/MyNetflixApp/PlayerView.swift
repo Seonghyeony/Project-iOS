4ad44e69d97d5129497060afe4804599ddd0456a
Seonghyeony <sunghyun7949@naver.com>
2021-01-15 22:00:22 +0900

    Feat: AVPlayLayer, PlayerView, ControlView

--- MyNetflixApp/MyNetflixApp/PlayerView.swift
+++ MyNetflixApp/MyNetflixApp/PlayerView.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 com.joonwon. All rights reserved.
 //
 
+// AVPlayer에서 나오는 visual output을 담당하는 녀석이 AVPlayerlayer이다.
+// 이거를 쓰려면 밑의 방식으로 쓰라고 document에 나와있다.
 // https://developer.apple.com/documentation/avfoundation/avplayerlayer
 
 import UIKit
--- MyNetflixApp/MyNetflixApp/PlayerViewController.swift
+++ MyNetflixApp/MyNetflixApp/PlayerViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
+    
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var controlView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    let player = AVPlayer()
     
     // 강제 landspace
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -19,15 +26,20 @@ class PlayerViewController: UIViewController {
         super.viewDidLoad()
     }
     
-
+    
+    @IBAction func togglePlaybutton(_ sender: Any) {
+        playButton.isSelected = !playButton.isSelected
+    }
+    
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: false, completion: nil)
     }
 }
 
-//extension AVPlayer {
-//    var isPlaying: Bool {
-//        guard self.currentItem != nil else { return false }
-//        return self.rate != 0
-//    }
-//}
+// 플레이 중인지 아닌지
+extension AVPlayer {
+    var isPlaying: Bool {
+        guard self.currentItem != nil else { return false }
+        return self.rate != 0
+    }
+}
