c4667c65e99e9246d36a74e8d87fb4a7a69f4b92
Seonghyeony <sunghyun7949@naver.com>
2021-01-17 18:51:14 +0900

    Feat: connecting RealtimeFirebase

--- Firebase101/Firebase101/ViewController.swift
+++ Firebase101/Firebase101/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+    let db = Database.database().reference()
 
+    @IBOutlet weak var dataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateLabel()
     }
-
-
+    
+    func updateLabel() {
+        db.child("firstData").observeSingleEvent(of: .value) { snapshot in
+            print("---> \(snapshot)")
+            
+            let value = snapshot.value as? String ?? ""
+            
+            DispatchQueue.main.async {
+                self.dataLabel.text = value
+            }
+        }
+    }
 }
 
