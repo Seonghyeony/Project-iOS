e64306df3b2935e29849e3b1b7fedd29ca398b70
Seonghyeony <sunghyun7949@naver.com>
2021-01-20 17:29:20 +0900

    _

---
+++ MyNetflixApp/MyNetflixApp/UpComingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpComingViewController.swift
+//  MyNetflixApp
+//
+//  Created by 임성현 on 2021/01/20.
+//  Copyright © 2021 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class UpComingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
